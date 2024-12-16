0331ab49fae207fbdb93482fa0d1444a020ad478
anna-dahlberg <anna.dahlberg@live.se>
2024-12-16 19:22:07 +0100

    added a test page for html

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+require(__DIR__ . '/vendor/autoload.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/styles/styles.css">
+    <link rel="stylesheet" href="https://use.typekit.net/roz6xva.css">
+    <title>The Selkies Rest</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
