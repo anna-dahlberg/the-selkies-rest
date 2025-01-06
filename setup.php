61599465d38c301b0d53d40cca04648911364973
anna-dahlberg <anna.dahlberg@live.se>
2025-01-06 21:27:11 +0100

    required room file in setup.php

--- setup.php
+++ setup.php
@@ -5,3 +5,5 @@ declare(strict_types=1);
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/calendar.php');
+
+$rooms = require_once(__DIR__ . '/rooms.php');
