720a6f0ec3696ae339c5096510ec6c4e8ed45446
anna-dahlberg <anna.dahlberg@live.se>
2025-01-07 13:30:26 +0100

    separated description

--- rooms.php
+++ rooms.php
@@ -5,19 +5,22 @@ declare(strict_types=1);
 return [
     1 => [
         'name' => 'The Nook',
-        'description' => 'Cozy and practical, our Budget Room provides the perfect sanctuary for the thrifty traveler. Compact yet comfortable, it’s thoughtfully designed with all the essentials for a restful stay. Whether you’re here for a short stopover or a quick getaway, you’ll find everything you need to recharge and relax. The simplicity of this room is complemented by clean lines and a warm, inviting atmosphere.',
+        'description1' => 'Cozy and practical, our Budget Room provides the perfect sanctuary for the thrifty traveler. Compact yet comfortable, it’s thoughtfully designed with all the essentials for a restful stay.',
+        'description2' => 'Whether you’re here for a short stopover or a quick getaway, you’ll find everything you need to recharge and relax. The simplicity of this room is complemented by clean lines and a warm, inviting atmosphere.',
         'image' => 'assets/images/theNook.png',
         'calendar' => new Calendar(1)
     ],
     2 => [
         'name' => 'The Refuge',
-        'description' => 'Enjoy the perfect mix of comfort and convenience in our Standard Room. Spacious enough for relaxation yet cozy enough to feel like home, this room is designed with modern amenities and tasteful décor. It’s the ideal choice for travelers seeking a bit more style and sophistication without going overboard. Every detail is crafted to make your stay as smooth and enjoyable as possible.',
+        'description1' => 'Enjoy the perfect mix of comfort and convenience in our Standard Room. Spacious enough for relaxation yet cozy enough to feel like home, this room is designed with modern amenities and tasteful décor.',
+        'description2' => 'It’s the ideal choice for travelers seeking a bit more style and sophistication without going overboard. Every detail is crafted to make your stay as smooth and enjoyable as possible.',
         'image' => '/assets/images/theRefuge.png',
         'calendar' => new Calendar(2)
     ],
     3 => [
         'name' => 'The Haven',
-        'description' => 'Step into indulgence with our Luxury Room. This spacious retreat is adorned with elegant furnishings and luxurious touches to make your stay unforgettable. Sink into plush bedding, take in the beautifully appointed décor, and enjoy premium amenities designed for the discerning guest.It’s not just a room; it’s an experience—one that transforms your trip into a truly memorable escape.',
+        'description1' => 'Step into indulgence with our Luxury Room. This spacious retreat is adorned with elegant furnishings and luxurious touches to make your stay unforgettable. Sink into plush bedding, take in the beautifully appointed décor, and enjoy premium amenities designed for the discerning guest.',
+        'description2' => 'It’s not just a room; it’s an experience—one that transforms your trip into a truly memorable escape.',
         'image' => '/assets/images/theHaven.png',
         'calendar' => new Calendar(3)
     ]
