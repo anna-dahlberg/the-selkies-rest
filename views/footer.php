5b268c5bc898304a8f77fbafc102ddd897498e0a
anna-dahlberg <anna.dahlberg@live.se>
2025-01-09 13:45:46 +0100

    styled footer

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,40 @@
 <footer>
-    <div class="footer"></div>
+    <div class="footer">
+        <div class="footerImgContainer">
+            <img src="assets/images/logo2.png" alt="">
+        </div>
+        <div class="footerContainer">
+            <ul>
+                <h5>Find us</h5>
+                <li>The Selkie’s Rest <br>
+                    1 Mythical Haven Road <br>
+                    Blackthorn Isle, Argyll <br>
+                    Scotland, UK <br>
+                    IM4 5EL</li>
+            </ul>
+            <ul>
+                <h5>Contact us</h5>
+                <li><a href="#">Contact</a></li>
+                <li>Email:<br>
+                    <a href="mailto:theselkiesrest@mail.com">theselkiesrest@mail.com</a>
+                </li>
+            </ul>
+            <ul>
+                <h5>About us</h5>
+                <li><a href="#">About Blackthorn Isle</a></li>
+                <li><a href="#">About the hotel</a></li>
+                <li><a href="#">Mythology</a></li>
+                <li><a href="#">Career</a></li>
+            </ul>
+            <ul>
+                <h5>Follow us</h5>
+                <li class="socialMedia"><img src="assets/images/facebook.png" alt=""><a href="#">Facebook</a></li>
+                <li class="socialMedia"><img src="assets/images/instagram.png" alt=""><a href="#">Instagram</a></li>
+                <li class="socialMedia"><img src="assets/images/youtube.png" alt=""><a href="#">Youtube</a></li>
+            </ul>
+
+        </div>
+    </div>
 </footer>
 
 </body>
